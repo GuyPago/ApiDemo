dfd29f631517d94ec51894d0323b3912af594e5c
Guy Taggar  - Desktop <guypago@outlook.com>
2022-04-29 17:30:02 +0300

    Add CRUD

--- ApiDemo/Repositores/IItemsRepository.cs
+++ ApiDemo/Repositores/IItemsRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ApiDemo.Controllers
+﻿using ApiDemo.Models;
+
+namespace ApiDemo.Repositores
 {
     public interface IItemsRepository
     {
+        public IEnumerable<Item> GetItems();
+        public Item GetItem(Guid id);
+        public void CreateItem(Item item);
+        public void UpdateItem(Item item);
+        public void DeleteItem(Guid id);
     }
 }
