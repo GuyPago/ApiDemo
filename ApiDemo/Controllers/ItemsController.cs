39dec59e0887e6630d74442353f471ca969eabcb
Guy Taggar  - Desktop <guypago@outlook.com>
2022-04-29 18:06:10 +0300

    Change Item to ItemDto

--- ApiDemo/Controllers/ItemsController.cs
+++ ApiDemo/Controllers/ItemsController.cs
@@ -1,6 +1,32 @@
-﻿namespace ApiDemo.Controllers
+﻿using ApiDemo.Dtos;
+using ApiDemo.Models;
+using ApiDemo.Repositores;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemo.Controllers
 {
-    public class ItemsController
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
     {
+        private readonly IItemsRepository? _repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet] // Get /Item
+        public IEnumerable<ItemDto> GetItems()
+        {
+            return _repository.GetItems().Select(x => x.AsDto());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItem(Guid id)
+        {
+            Item? item = _repository.GetItem(id);
+            return item == null ? NotFound() : item.AsDto();
+        }
     }
 }
