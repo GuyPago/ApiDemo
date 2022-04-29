5ee9e4e33d7275191e1360ffe0a06e94b6cd490b
Guy Taggar  - Desktop <guypago@outlook.com>
2022-04-29 18:02:21 +0300

    Add AsDto extension

---
+++ ApiDemo/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using ApiDemo.Dtos;
+using ApiDemo.Models;
+
+namespace ApiDemo
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto(this Item item)
+        {
+            return new ItemDto
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Price = item.Price,
+                CreatedDate = item.CreatedDate
+            };
+        }
+    }
+}
