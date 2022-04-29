c857d8c9daee34bcf0aa7b032f96674831af331e
Guy Taggar  - Desktop <guypago@outlook.com>
2022-04-29 17:54:48 +0300

    Add GetItem and GetItems methods

--- ApiDemo/Repositores/IItemsRepository.cs
+++ ApiDemo/Repositores/IItemsRepository.cs
@@ -5,7 +5,7 @@ namespace ApiDemo.Repositores
     public interface IItemsRepository
     {
         public IEnumerable<Item> GetItems();
-        public Item GetItem(Guid id);
+        public Item? GetItem(Guid id);
         public void CreateItem(Item item);
         public void UpdateItem(Item item);
         public void DeleteItem(Guid id);
--- ApiDemo/Repositores/InMemoryRepository.cs
+++ ApiDemo/Repositores/InMemoryRepository.cs
@@ -1,6 +1,40 @@
-﻿namespace ApiDemo.Repositores
+﻿using ApiDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemo.Repositores
 {
-    public class InMemoryRepository : IItemsRepository
-    {
+    public class InMemoryRepository : IItemsRepository 
+    { 
+    
+        private List<Item> _items = new List<Item>()
+        {
+            new Item() { Id = Guid.NewGuid(), Name = "Potion", Price = 8, CreatedDate = DateTimeOffset.UtcNow},
+            new Item() { Id = Guid.NewGuid(), Name = "Betanir's Sword", Price = 49, CreatedDate = DateTimeOffset.UtcNow},
+            new Item() { Id = Guid.NewGuid(), Name = "Guy's Shield", Price = 36, CreatedDate = DateTimeOffset.UtcNow},
+            new Item() { Id = Guid.NewGuid(), Name = "Ram's Belly", Price = 55, CreatedDate = DateTimeOffset.UtcNow},
+        };
+        public IEnumerable<Item> GetItems()
+        {
+            return _items;
+        }
+        public Item? GetItem(Guid id)
+        {
+            Item? item = _items.SingleOrDefault(x => x.Id == id);
+            return item;
+        }
+        public void CreateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
