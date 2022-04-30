b69d874faaa43c1ddf847497dd179ea937f90f47
Guy Taggar  - Desktop <guypago@outlook.com>
2022-04-30 12:42:18 +0300

    Add MongoDb settings class

---
+++ ApiDemo/Settings/MongoDbSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiDemo.Settings
+{
+    public class MongoDbSettings
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string connectionString
+        {
+            get { return $"mongodb://{Host}:{Port}"; }
+        }
+
+    }
+}
