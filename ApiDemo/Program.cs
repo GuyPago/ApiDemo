766c29fbafbd44f10aab7fce22fa2386c36f9947
Guy Taggar  - Desktop <guypago@outlook.com>
2022-04-30 13:45:48 +0300

    modify mongo connection settings

--- ApiDemo/Program.cs
+++ ApiDemo/Program.cs
@@ -1,5 +1,7 @@
 using ApiDemo.Repositores;
 using ApiDemo.Settings;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +12,8 @@ builder.Services.AddSingleton<IMongoClient>(ServiceProvider =>
     var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
     return new MongoClient(settings.connectionString);
 });
-
+BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
 builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
